d545606efded04259a7e62d52ae954495c70c810
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-09-16 20:07:21 -0300

    adjust games route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express, { request } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient({
+    log: ['query']
+})
 
-app.get('/games', (req, res) => {
-    return res.json([])
+app.get('/games', async (req, res) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
+    return res.json(games)
 })
 
 app.post('/ads', (req, res) => {
